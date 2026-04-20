eb071cc8c6fa5ca5541ffc377b45c381df66312a
unknown <mh.z77w@gmail.com>
2026-04-20 11:02:33 +0330

    feat(auth): implement OTP sending with temproroy in-memory storage

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,11 +2,22 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AuthService {
-  sendOtp(phone: string) {
-    return phone;
+  otps = new Map();
+  async sendOtp(phone: string) {
+    const code = this.generateOtp();
+    this.otps.set(phone, {
+      code,
+      expireAt: Date.now() + 1000 * 2 * 60, //2m
+    });
+    await this.sendSms(phone, code);
+    return { success: true };
   }
 
   generateOtp() {
     return Math.floor(100000 + Math.random() * 900000).toString();
+  }
+  async sendSms(phone: string, code: string) {
+    // TODO: sms api from sms.ir
+    console.log('SMS to', phone, 'OTP:', code);
   }
 }
