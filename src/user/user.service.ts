be7f0d08244408fc01dc81735dd40be6deddb6f6
unknown <mh.z77w@gmail.com>
2026-04-22 12:44:36 +0330

    feat(user.service): implement findByPhone create and save methods

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private repo: Repository<User>,
+  ) {}
+  async findByPhone(phone: string) {
+    return this.repo.findOne({ where: { phone } });
+  }
+
+  async create(phone: string) {
+    const user = this.repo.create({ phone });
+    return this.repo.save(user);
+  }
+
+  async save(user: User) {
+    return this.repo.save(user);
+  }
+}
