43f432e4d8b221a817ab882aac87089d358d9529
unknown <mh.z77w@gmail.com>
2026-04-23 17:33:12 +0330

    feat(product): create product entity

---
+++ src/product/entities/product.entity.ts
@@ -0,0 +1,49 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { MetalType } from '../enums/metalType.enum';
+import { PricingType } from '../enums/pricingType.enum';
+import { User } from 'src/entities/user.entity';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column({
+    type: 'enum',
+    enum: MetalType,
+  })
+  metalType: MetalType;
+
+  @Column({
+    type: 'enum',
+    enum: PricingType,
+  })
+  pricingType: PricingType;
+
+  @Column('decimal', {
+    precision: 15,
+    scale: 3,
+  })
+  price: number;
+
+  @ManyToMany(() => User)
+  @JoinTable()
+  brokers: User[];
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
