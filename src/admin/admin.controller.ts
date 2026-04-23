f57f29c7f44817dee3f91d1570b1fa2118439a60
unknown <mh.z77w@gmail.com>
2026-04-23 15:41:02 +0330

    feat(admin.controller): implement updateUserRole for updating user role and apply jwt and roles guards

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Patch, UseGuards } from '@nestjs/common';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { UserRole } from 'src/enums/user-role.enum';
+import { UpdateUserRoleDto } from 'src/user/dtos/updateUserRole.dto';
+import { AdminService } from './admin.service';
 
 @Controller('admin')
-export class AdminController {}
+@Roles(UserRole.ADMIN)
+@UseGuards(JwtAuthGuard, RolesGuard)
+export class AdminController {
+  constructor(private adminService: AdminService) {}
+
+  @Patch('users/role')
+  updateUserRole(@Body() { id, role }: UpdateUserRoleDto) {
+    return this.adminService.updateUserRole(id, role);
+  }
+}
