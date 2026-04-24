bd4da89c8407fc6c38e316971b6019c611cd97b7
unknown <mh.z77w@gmail.com>
2026-04-24 15:53:44 +0330

    feat: set up swagger

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { UserService } from './user/user.service';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,15 @@ async function bootstrap() {
   const userService = app.get(UserService);
 
   await userService.createFirstAdmin(process.env.ADMIN_PHONE || '09221024578');
+
+  const config = new DocumentBuilder()
+    .setTitle('Cgold API')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
