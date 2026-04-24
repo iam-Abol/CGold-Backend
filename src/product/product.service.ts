0e546d7d0c892f5e36fb6b76ac20740f82b14a83
unknown <mh.z77w@gmail.com>
2026-04-24 16:19:34 +0330

    feat(product.service): implement update --> CRUD FINISHED

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,15 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Product } from './entities/product.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateProductDto } from './dtos/createProduct.dto';
 import { UpdateProductDto } from './dtos/updateProduct.dto';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class ProductService {
   constructor(
     @InjectRepository(Product)
     private productRepo: Repository<Product>,
+    private userService: UserService,
   ) {}
   create(dto: CreateProductDto) {
     const product = this.productRepo.create({
@@ -28,8 +30,18 @@ export class ProductService {
     });
   }
   async update(id: string, dto: UpdateProductDto) {
+    const { brokerIds, ...updateData } = dto;
+
     const product = await this.findOne(id);
     if (!product) throw new NotFoundException();
+
+    if (brokerIds) {
+      product.brokers = await this.userService.findByIds(
+        brokerIds.map((id) => Number(id)),
+      );
+    }
+    Object.assign(product, updateData);
+    return this.productRepo.save(product);
   }
   async delete(id: string) {
     return this.productRepo.delete(id);
