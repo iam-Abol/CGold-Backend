b1f0cf5efd4b1930c5341e77a769281e79dedef6
unknown <mh.z77w@gmail.com>
2026-04-24 16:20:43 +0330

    fix(product): fix nest DI error

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from './entities/product.entity';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product])],
+  imports: [TypeOrmModule.forFeature([Product]), UserModule],
   providers: [ProductService],
 })
 export class ProductModule {}
