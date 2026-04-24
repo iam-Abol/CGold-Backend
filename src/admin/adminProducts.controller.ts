21b7a4ae836e5563130f322f198857df0a439410
unknown <mh.z77w@gmail.com>
2026-04-24 16:33:23 +0330

    chore(admin): initialize adminProduct

---
+++ src/admin/adminProducts.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, UseGuards } from '@nestjs/common';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { UserRole } from 'src/enums/user-role.enum';
+import { ProductService } from 'src/product/product.service';
+
+@Controller('admin/products')
+@UseGuards(JwtAuthGuard, RolesGuard)
+@Roles(UserRole.ADMIN)
+export class AdminProductsController {
+  constructor(private readonly productService: ProductService) {}
+}
