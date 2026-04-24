b1dc45e1651efd6422045328be69b88f35d6892a
unknown <mh.z77w@gmail.com>
2026-04-24 16:39:15 +0330

    feat(adminProductController): implement create route

--- src/admin/adminProducts.controller.ts
+++ src/admin/adminProducts.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { Roles } from 'src/auth/decorators/roles.decorator';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
 import { UserRole } from 'src/enums/user-role.enum';
+import { CreateProductDto } from 'src/product/dtos/createProduct.dto';
 import { ProductService } from 'src/product/product.service';
 
 @Controller('admin/products')
@@ -10,4 +11,9 @@ import { ProductService } from 'src/product/product.service';
 @Roles(UserRole.ADMIN)
 export class AdminProductsController {
   constructor(private readonly productService: ProductService) {}
+
+  @Post()
+  create(@Body() dto: CreateProductDto) {
+    return this.productService.create(dto);
+  }
 }
