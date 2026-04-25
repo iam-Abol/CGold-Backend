a3017d3ebaf93f3a2a4116b404628f5fb3c73852
unknown <mh.z77w@gmail.com>
2026-04-25 18:09:16 +0330

    feat(broker.controller): add getProducts endpoint

--- src/broker/broker.controller.ts
+++ src/broker/broker.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { BrokerService } from './broker.service';
 
 @Controller('broker')
-export class BrokerController {}
+export class BrokerController {
+  constructor(private brokerService: BrokerService) {}
+  @Get(':brokerId/products')
+  getProducts(@Param('brokerId') brokerId: string) {
+    return this.brokerService.getProductsForBroker(brokerId);
+  }
+}
--- src/broker/broker.service.ts
+++ src/broker/broker.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class BrokerService {}
+export class BrokerService {
+  getProductsForBroker(brokerId: string) {}
+}
