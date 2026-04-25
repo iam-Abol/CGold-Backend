5fdb87fcf95724bedaf4a542c1a07f6ef942e7cb
unknown <mh.z77w@gmail.com>
2026-04-25 21:24:15 +0330

    feat(swagger): add apiProperty to refresh.dto.ts

--- src/auth/dtos/refresh.dto.ts
+++ src/auth/dtos/refresh.dto.ts
@@ -1,10 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString, IsNumberString } from 'class-validator';
 
 export class RefreshTokenDto {
+  @ApiProperty({
+    example: 1,
+    description: 'User ID belonging to the refresh token',
+  })
   @IsNotEmpty()
   @IsNumberString()
   userId: string;
 
+  @ApiProperty({
+    example: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...',
+    description: 'Valid refresh token previously issued to the user',
+  })
   @IsNotEmpty()
   @IsString()
   token: string;
