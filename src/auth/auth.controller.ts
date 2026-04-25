fd5527b669c6388ea3adf4bc14f1a9ae070bb586
unknown <mh.z77w@gmail.com>
2026-04-25 21:09:33 +0330

    feat(swagger): document auth sendOtp endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,11 +3,15 @@ import { AuthService } from './auth.service';
 import { VerifyOtpDto } from './dtos/verifyOtp.dto';
 import { SendOtpDto } from './dtos/sendOtp.dto';
 import { RefreshTokenDto } from './dtos/refresh.dto';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
+
   @Post('/send-otp')
+  @ApiOperation({ summary: 'Send otp to user' })
+  @ApiResponse({ status: 200, description: '{success: true}' })
   sendOtp(@Body() { phone }: SendOtpDto) {
     return this.authService.sendOtp(phone);
   }
--- src/auth/dtos/sendOtp.dto.ts
+++ src/auth/dtos/sendOtp.dto.ts
@@ -1,6 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsPhoneNumber } from 'class-validator';
 
 export class SendOtpDto {
+  @ApiProperty({ example: '09120000000' })
   @IsPhoneNumber('IR')
   phone: string;
 }
