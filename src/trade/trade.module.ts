7bdf8b9c02233da1fabb08b2224a8c0142fc32a1
unknown <mh.z77w@gmail.com>
2026-04-27 22:57:45 +0330

    feat(trade): import required modules

--- src/trade/trade.module.ts
+++ src/trade/trade.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { TradeService } from './trade.service';
 import { TradeController } from './trade.controller';
+import { ProductModule } from 'src/product/product.module';
+import { UserModule } from 'src/user/user.module';
+import { AuthModule } from 'src/auth/auth.module';
+import { Trade } from './entities/trade.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   providers: [TradeService],
-  controllers: [TradeController]
+  controllers: [TradeController],
+  imports: [
+    TypeOrmModule.forFeature([Trade]),
+    ProductModule,
+    UserModule,
+    AuthModule,
+  ],
 })
 export class TradeModule {}
--- src/trade/trade.service.ts
+++ src/trade/trade.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { ProductService } from 'src/product/product.service';
+import { UserService } from 'src/user/user.service';
+import { Repository } from 'typeorm';
+import { Trade } from './entities/trade.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class TradeService {}
+export class TradeService {
+  constructor(
+    @InjectRepository(Trade)
+    private tradeRepo: Repository<Trade>,
+
+    private productService: ProductService,
+
+    private userService: UserService,
+  ) {}
+  
+}
