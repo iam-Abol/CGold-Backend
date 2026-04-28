15b7506283ccb36a7a2b630bb85265d90b59f0bb
unknown <mh.z77w@gmail.com>
2026-04-28 19:05:54 +0330

    feat(trade.controller): add getMyTrades route for getting a users requested trades

--- src/trade/trade.controller.ts
+++ src/trade/trade.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { TradeService } from './trade.service';
 import { CreateTradeDto } from './dtos/createTrade.dto';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
@@ -13,5 +13,9 @@ export class TradeController {
     // console);
 
     return this.tradeService.createTrade(String(req.user.id), dto);
+  }
+  @Get('my')
+  async getMyTrades(@Req() req: any) {
+    return this.tradeService.findAllByUser(String(req.user.id));
   }
 }
