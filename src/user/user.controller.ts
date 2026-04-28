b49494a1d7792558e9fc5eb04c765e1ebafe6f0b
unknown <mh.z77w@gmail.com>
2026-04-28 17:28:39 +0330

    feat(user.controller): add setActive route

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,22 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { Roles } from 'src/auth/decorators/roles.decorator';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
 import { UserRole } from 'src/enums/user-role.enum';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
+  constructor(private userService: UserService) {}
+
   @Get('admin')
   @Roles(UserRole.ADMIN)
   @UseGuards(JwtAuthGuard, RolesGuard)
@@ -18,5 +29,12 @@ export class UserController {
   @UseGuards(JwtAuthGuard, RolesGuard)
   userRoute() {
     return 'Welcome User!';
+  }
+
+  @Patch('/active')
+  @Roles(UserRole.BROKER, UserRole.ADMIN)
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  setActive(@Req() req: any, @Body('isActive') isActive: boolean) {
+    return this.userService.setActiveStatus(req.user.id, isActive);
   }
 }
