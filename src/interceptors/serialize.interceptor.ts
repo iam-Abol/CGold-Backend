a035b439396473bdfb141caee094ca54fe3f3396
unknown <mh.z77w@gmail.com>
2026-05-15 13:34:31 +0330

    feat: add Serialize decorator to apply SerializeInterceptor with DTO

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -9,6 +9,10 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { plainToInstance } from 'class-transformer';
 
+export function Serialize(dto: Type<any>) {
+  return UseInterceptors(new SerializeInterceptor(dto));
+}
+
 export class SerializeInterceptor implements NestInterceptor {
   constructor(private dto: Type<any>) {}
 
