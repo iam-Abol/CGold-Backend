8596448699d59ee6f5c93898796a06faa1e07a1d
unknown <mh.z77w@gmail.com>
2026-05-15 13:32:59 +0330

    feat(interceptor): add SerializeInterceptor for response transformation using class-transformer

---
+++ src/interceptors/serialize.interceptor.ts
@@ -0,0 +1,24 @@
+import {
+  UseInterceptors,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+  Type,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { plainToInstance } from 'class-transformer';
+
+export class SerializeInterceptor implements NestInterceptor {
+  constructor(private dto: Type<any>) {}
+
+  intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
+    return handler.handle().pipe(
+      map((data: any) => {
+        return plainToInstance(this.dto, data, {
+          excludeExtraneousValues: true,
+        });
+      }),
+    );
+  }
+}
