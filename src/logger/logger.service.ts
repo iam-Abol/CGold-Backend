d5f83edca1577add14d706dcc51eef66149e182e
Atlas <Atlas@DESKTOP-N4P751L>
2026-06-11 21:47:19 +0330

    removing trade entity(switching to a new logging method)

--- src/logger/logger.service.ts
+++ src/logger/logger.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { Trade } from 'src/trade/entities/trade.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class LoggerService {}
+export class LoggerService {
+    constructor(
+        @InjectRepository(User)
+        private readonly userRepository: Repository<User>,
+      ) {}
+      
+    async saveLog(trade :Trade){
+
+    }
+}
