be65291a6fc017c2a6765ccf835fb7f41b7f98b3
unknown <mh.z77w@gmail.com>
2026-06-12 16:55:57 +0330

    scheduler setup

--- src/market/market.module.ts
+++ src/market/market.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MarketService } from './market.service';
 import { MarketController } from './market.controller';
+import { PriceSchedulerService } from './schedulers/price-scheduler/price-scheduler.service';
 
 @Module({
-  providers: [MarketService],
+  providers: [MarketService, PriceSchedulerService],
   controllers: [MarketController]
 })
 export class MarketModule {}
