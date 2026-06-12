2f82436ca52a514794a165f06e2157ae9811d741
unknown <mh.z77w@gmail.com>
2026-06-12 17:10:04 +0330

    feat(market): implement cron job in price scheduler service for getting prices every 30 seconds

--- src/market/market.module.ts
+++ src/market/market.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { MarketService } from './market.service';
 import { MarketController } from './market.controller';
 import { PriceSchedulerService } from './schedulers/price-scheduler/price-scheduler.service';
+import { PriceProviderService } from './price-provider.service';
+
+import { ScheduleModule } from '@nestjs/schedule';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from 'src/product/entities/product.entity';
 
 @Module({
-  providers: [MarketService, PriceSchedulerService],
-  controllers: [MarketController]
+  imports: [ScheduleModule.forRoot(), TypeOrmModule.forFeature([Product])],
+  controllers: [MarketController],
+  providers: [MarketService, PriceSchedulerService, PriceProviderService],
 })
 export class MarketModule {}
--- src/market/schedulers/price-scheduler/price-scheduler.service.ts
+++ src/market/schedulers/price-scheduler/price-scheduler.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { Cron } from '@nestjs/schedule';
+import { InjectRepository } from '@nestjs/typeorm';
+import { PriceProviderService } from 'src/market/price-provider.service';
+import { Product } from 'src/product/entities/product.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class PriceSchedulerService {}
+export class PriceSchedulerService {
+  constructor(private readonly provider: PriceProviderService) {}
+
+  @Cron('*/30 * * * * *')
+  async updatePrices() {
+    const goldOuncePrice = await this.provider.getGoldPrice();
+    const silverOuncePrice = await this.provider.getSilverPrice();
+
+    // todo: update db
+    console.log(goldOuncePrice, 'gold');
+  }
+}
