77e2180698054939b77087669ada50f5f5d6bec6
unknown <mh.z77w@gmail.com>
2026-06-12 17:41:49 +0330

    feat(priceProviderService): implement calculatePrice function

--- src/market/schedulers/price-scheduler/price-scheduler.service.ts
+++ src/market/schedulers/price-scheduler/price-scheduler.service.ts
@@ -3,18 +3,32 @@ import { Cron } from '@nestjs/schedule';
 import { InjectRepository } from '@nestjs/typeorm';
 import { PriceProviderService } from 'src/market/price-provider.service';
 import { Product } from 'src/product/entities/product.entity';
+import { MetalType } from 'src/product/enums/metalType.enum';
 import { Repository } from 'typeorm';
 
 @Injectable()
 export class PriceSchedulerService {
   constructor(private readonly provider: PriceProviderService) {}
 
-  @Cron('*/30 * * * * *')
+  @Cron('*/300 * * * * *')
   async updatePrices() {
     const goldOuncePrice = await this.provider.getGoldPrice();
     const silverOuncePrice = await this.provider.getSilverPrice();
+    const usdToToman = await this.provider.getUsdToToman();
 
     // todo: update db
-    console.log(goldOuncePrice, 'gold');
+    console.log(usdToToman, 'toman');
+  }
+
+  private calculatePrice(
+    product: Product,
+    goldPerGramUSD: number,
+    silverPerGramUSD: number,
+    usdToToman: number,
+  ) {
+    const baseUSD =
+      product.metalType === MetalType.GOLD ? goldPerGramUSD : silverPerGramUSD;
+
+    return baseUSD * usdToToman * product.weight * product.purity;
   }
 }
