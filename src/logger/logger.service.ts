204eaade6e5f8b58d24df6e674b880c3e6dae507
Atlas <Atlas@DESKTOP-N4P751L>
2026-06-14 01:16:44 +0330

    feat(logger.service) queryLog method

--- src/logger/logger.service.ts
+++ src/logger/logger.service.ts
@@ -3,15 +3,49 @@ import { Trade } from 'src/trade/entities/trade.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/entities/user.entity';
 import { Repository } from 'typeorm';
+import { TradeQueryDto } from './dtos/tradeQuery.dto';
 
 @Injectable()
 export class LoggerService {
     constructor(
-        @InjectRepository(User)
-        private readonly userRepository: Repository<User>,
+        @InjectRepository(Trade)
+        private readonly repo: Repository<Trade>,
       ) {}
       
-    async saveLog(trade :Trade){
-
-    }
+      async findTrades(filter: TradeQueryDto) {
+        const { userId, brokerId, fromDate, toDate } = filter;
+      
+        const query = this.repo.createQueryBuilder('trade');
+      
+        if (userId !== undefined) {
+          query.andWhere('trade.User.id = :userId', {
+            userId,
+          });
+        }
+      
+        if (brokerId !== undefined) {
+          query.andWhere('trade.Broker.id = :brokerId', {
+            brokerId,
+          });
+        }
+      
+        if (fromDate) {
+          query.andWhere(
+            'trade.createdAt >= :fromDate',
+            { fromDate },
+          );
+        }
+      
+        if (toDate) {
+          query.andWhere(
+            'trade.createdAt <= :toDate',
+            { toDate },
+          );
+        }
+      
+        return query
+          .orderBy('trade.createdAt', 'DESC')
+          .getMany();
+      }
+    
 }
