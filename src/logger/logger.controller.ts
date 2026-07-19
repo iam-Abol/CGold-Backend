f2250259fc62df03078ccc595859c8f74cfefad7
Atlas <Atlas@DESKTOP-N4P751L>
2026-07-19 15:15:02 +0330

    fix(logger) fixing logger controller, service and findTrade function

--- src/logger/logger.controller.ts
+++ src/logger/logger.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { LoggerService } from './logger.service';
+import { TradeQueryDto } from './dtos/tradeQuery.dto';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { IsProfileComplete } from 'src/auth/guards/isProfileComplete.guard';
+import { Roles } from 'src/auth/decorators/roles.decorator';
 
 @Controller('logger')
-export class LoggerController {}
+export class LoggerController {
+
+  constructor(private loggerService: LoggerService) {}
+  
+  @Get()
+  @UseGuards(RolesGuard, IsProfileComplete)
+      completeProfile(@Req() req: any, @Body() body: TradeQueryDto) {
+        console.log('finding trades');
+        return this.loggerService.findTrades(body);
+      }
+}
--- src/logger/logger.module.ts
+++ src/logger/logger.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { LoggerService } from './logger.service';
 import { LoggerController } from './logger.controller';
 import { TradeModule } from 'src/trade/trade.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Trade } from 'src/trade/entities/trade.entity';
 
 @Module({
   providers: [LoggerService],
   controllers: [LoggerController],
-  imports: [TradeModule],
+  imports: [TradeModule,
+            TypeOrmModule.forFeature([Trade])
+  ],
 })
 export class LoggerModule {}
